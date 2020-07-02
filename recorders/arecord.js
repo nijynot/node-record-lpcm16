d656eb7558f069d26bbeaef1d5db6449a10840c1
Tony Jin <nijynot@gmail.com>
2020-07-02 17:38:15 +0200

    Add support for dplug

--- recorders/arecord.js
+++ recorders/arecord.js
@@ -1,18 +1,20 @@
 // On some systems (RasPi), arecord is the prefered recording binary
 module.exports = (options) => {
-  const cmd = 'arecord'
+  const cmd = 'arecord';
 
   const args = [
-    '-q', // show no progress
+    // '-q', // show no progress
+    '-f', 'S16_LE', // Sample format
     '-r', options.sampleRate, // sample rate
     '-c', options.channels, // channels
-    '-t', options.audioType, // audio type
-    '-f', 'S16_LE', // Sample format
-    '-' // pipe
+    // '-t', options.audioType, // audio type
+    // '-' // pipe
   ]
 
-  if (options.device) {
-    args.unshift('-D', options.device)
+  if (options.dplug) {
+    args.unshift(options.dplug);
+  } else if (options.device) {
+    args.unshift('-D', options.device);
   }
 
   return { cmd, args }
